9599c6d6ef0ccbb4d207537b502e2f194fc258e2
Henry Wong <H_wong14@u.pacific.edu>
2019-07-04 22:10:15 -0700

    Added listview

--- app/src/main/java/com/example/henry/notes/MainActivity.java
+++ app/src/main/java/com/example/henry/notes/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.henry.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> notes = new ArrayList<String>();
+
+        notes.add("New Note!");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+
+        listView.setAdapter(arrayAdapter);
     }
 }
