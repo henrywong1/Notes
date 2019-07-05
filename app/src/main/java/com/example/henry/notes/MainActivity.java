69c1af7a15448775d0482af9a7bd66c51e30a2b4
Henry Wong <H_wong14@u.pacific.edu>
2019-07-05 11:22:34 -0700

    Added Menu

--- app/src/main/java/com/example/henry/notes/MainActivity.java
+++ app/src/main/java/com/example/henry/notes/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.henry.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,16 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
